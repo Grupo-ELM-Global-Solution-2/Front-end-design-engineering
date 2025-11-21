10b4af8b2540654d9b49c7ba0178a85974efe4ec
LuzBGouveia <lu.barros.gouveia2@gmail.com>
2025-11-21 15:46:11 -0300

    feat: Adicionando o path do FAQ

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import Avaliacao from './routes/Avaliacao/index.tsx';
 import Error from './routes/Error/index.tsx';
 import Integrantes from './routes/Integrantes/index.tsx';
 import Contato from './routes/Contato/index.tsx';
+import Faq from './routes/Faq/index.tsx';
 
 const router = createBrowserRouter([
   {path:"/", element:<App/>, errorElement:<Error/>, children:[
@@ -15,6 +16,7 @@ const router = createBrowserRouter([
     {path:"/avaliacao", element: <Avaliacao/>},
     {path:"/integrantes", element: <Integrantes/>},
     {path:"/contato", element: <Contato/>},
+    {path:"/faq", element: <Faq/>},
   ]}
 ]);
 
