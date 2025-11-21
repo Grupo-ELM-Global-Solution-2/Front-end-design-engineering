dfb0b0fe44afef6c464787cc35e63ab6a296597b
LuzBGouveia <lu.barros.gouveia2@gmail.com>
2025-11-21 15:42:54 -0300

    feat: Adicionando as rotas no main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './routes/Home/index.tsx';
 import Avaliacao from './routes/Avaliacao/index.tsx';
 import Error from './routes/Error/index.tsx';
+import Integrantes from './routes/Integrantes/index.tsx';
 
 const router = createBrowserRouter([
   {path:"/", element:<App/>, errorElement:<Error/>, children:[
     {path:"/", element: <Home/>},
     {path:"/avaliacao", element: <Avaliacao/>},
+    {path:"/integrantes", element: <Integrantes/>},
   ]}
 ]);
 
